32e9b661392cf35be0397c38a43a7f5154d94c4b
Michael Underwood <mike.underwood@gmail.com>
2021-11-16 11:35:42 -0700

    Initial commit of bare api/blazor combo app

---
+++ BlazoriseApp/Controllers/IndexController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazoriseApp.Controllers
+{
+    [Route("")]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet("")]
+        public string HelloWorld () => "Hello, world!";
+    }
+}
